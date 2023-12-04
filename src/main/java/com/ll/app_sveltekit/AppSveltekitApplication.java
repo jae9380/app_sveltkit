63226b486ecb3405c9c801bddf3ecfdc7ac3c2be
jae9380 <ljy5314@gmail.com>
2023-12-04 19:04:55 +0900

    setting

---
+++ src/main/java/com/ll/app_sveltekit/AppSveltekitApplication.java
@@ -0,0 +1,13 @@
+package com.ll.app_sveltekit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSveltekitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSveltekitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/app_sveltekit/AppSveltekitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.app_sveltekit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSveltekitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
